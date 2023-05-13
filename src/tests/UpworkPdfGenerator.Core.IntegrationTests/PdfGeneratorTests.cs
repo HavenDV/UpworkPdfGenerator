b24c907d191b9000d8f477bf01c76d19e8cf72aa
Konstantin S <havendv@gmail.com>
2023-05-13 18:26:38 +0400

    fix: Removed Windows projects.

--- src/tests/UpworkPdfGenerator.Core.IntegrationTests/PdfGeneratorTests.cs
+++ src/tests/UpworkPdfGenerator.Core.IntegrationTests/PdfGeneratorTests.cs
@@ -1,36 +1,36 @@
-using System.Diagnostics;
-using System.Globalization;
-
-namespace UpworkPdfGenerator.Core.Tests;
-
-[TestClass]
-public class PdfGeneratorTests
-{
-    [TestMethod]
-    public void GenerateTest()
-    {
-        using var destinationStream = new MemoryStream();
-        var signPngBytes = File.ReadAllBytes(@"D:\Backup\My\sign.png");
-
-        var date = DateTime.UtcNow;
-        PdfGenerator.GenerateConfirmationOfServicesForm(
-            destinationStream,
-            signPngBytes,
-            "ИП Стуков Константин Михайлович",
-            "Stukov Konstantin Mihaylovich (Individual entrepreneur)",
-            1278.14,
-            date);
-
-        var bytes = destinationStream.ToArray();
-        var path = Path.Combine(
-            Path.GetTempPath(),
-            $"Confirmation of Services Form - {date.ToString("MMMM dd, yyyy", CultureInfo.InvariantCulture)}.pdf");
-
-        File.WriteAllBytes(path, bytes);
-
-        Process.Start(new ProcessStartInfo("chrome.exe", $"\"{path}\"")
-        {
-            UseShellExecute = true,
-        });
-    }
-}
+// using System.Diagnostics;
+// using System.Globalization;
+//
+// namespace UpworkPdfGenerator.Core.Tests;
+//
+// [TestClass]
+// public class PdfGeneratorTests
+// {
+//     [TestMethod]
+//     public void GenerateTest()
+//     {
+//         using var destinationStream = new MemoryStream();
+//         var signPngBytes = File.ReadAllBytes(@"D:\Backup\My\sign.png");
+//
+//         var date = DateTime.UtcNow;
+//         PdfGenerator.GenerateConfirmationOfServicesForm(
+//             destinationStream,
+//             signPngBytes,
+//             "ИП Стуков Константин Михайлович",
+//             "Stukov Konstantin Mihaylovich (Individual entrepreneur)",
+//             1278.14,
+//             date);
+//
+//         var bytes = destinationStream.ToArray();
+//         var path = Path.Combine(
+//             Path.GetTempPath(),
+//             $"Confirmation of Services Form - {date.ToString("MMMM dd, yyyy", CultureInfo.InvariantCulture)}.pdf");
+//
+//         File.WriteAllBytes(path, bytes);
+//
+//         Process.Start(new ProcessStartInfo("chrome.exe", $"\"{path}\"")
+//         {
+//             UseShellExecute = true,
+//         });
+//     }
+// }
